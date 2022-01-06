72fe0ed68b6eb28c45d2ae77f8a97dc05e001c89
ridho hardiyanto <ridhoadha117@gmail.com>
2022-01-06 15:11:53 +0700

    init home screen
    prepare home screen

--- src/pages/SuccessSignUp/index.js
+++ src/pages/SuccessSignUp/index.js
@@ -3,17 +3,25 @@ import {StyleSheet, Text, View, Image} from 'react-native';
 import successImg from '../../assets/img/success-ilu.png';
 import {Button} from '../../components';
 
-const SuccessSignUp = () => {
+const SuccessSignUp = ({navigation}) => {
   return (
     <View style={styles.page}>
-      <View style={styles.container}>
+      <View style={styles.bannerContainer}>
         <Text style={styles.successTxt}>Success</Text>
         <Image style={styles.img} source={successImg} />
       </View>
       <View style={styles.contentContainer}>
-        <Text style={styles.contenTxt}>Yeay you're all set</Text>
-        <Text style={styles.contenTxt}>Now you can start the journey</Text>
-        <Button text="continue" color="#68FF33" textColor="#355C7D" />
+        <Text style={styles.textContent}>Yeay you're all set</Text>
+        <Text style={styles.textContent}>Now you can start the journey</Text>
+        <View style={styles.btnContainer}>
+          <Button
+            text="continue"
+            color="#68FF33"
+            textColor="#355C7D"
+            onPress={() => navigation.navigate('Home')}
+            style={styles.btn}
+          />
+        </View>
       </View>
     </View>
   );
@@ -25,7 +33,7 @@ const styles = StyleSheet.create({
   page: {
     flex: 1,
   },
-  container: {
+  bannerContainer: {
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -35,7 +43,7 @@ const styles = StyleSheet.create({
   },
   successTxt: {
     fontSize: 24,
-    color: '355C7D',
+    color: '#355C7D',
     fontWeight: 'bold',
   },
   contentContainer: {
@@ -43,10 +51,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#355C7D',
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 10,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
   },
-  contenTxt: {
+  textContent: {
     fontSize: 20,
     color: '#FFFFFF',
+  },
+  btnContainer: {
+    width: 300,
+    paddingTop: 50,
+    paddingHorizontal: 80,
   },
 });
